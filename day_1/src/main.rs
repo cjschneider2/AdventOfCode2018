145b1176d8fd68e21eba1075eee413a1e1b6b97d
Charles J. Schneider <cjschneider2@gmail.com>
2018-12-01 22:29:58 +0100

    Complete Day 1

---
+++ day_1/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input_str = include_str!("../input_part1.txt");
+
+    let part_1_answer = part_1(&input_str);
+    println!("Part 1: {}", part_1_answer);
+
+    let part_2_answer = part_2(&input_str); // NOTE: uses same input
+    println!("Part 2: {}", part_2_answer);
+}
+
+fn part_1(input_str: &str) -> i32 {
+    let mut frequency = 0;
+    for line in input_str.lines() {
+        let delta_frequency: i32 = line.parse().unwrap();
+        frequency += delta_frequency;
+    }
+    frequency
+}
+
+fn part_2(input_str: &str) -> i32 {
+    let mut freq_set = HashSet::new();
+
+    let mut frequency = 0;
+    freq_set.insert(frequency);
+
+    loop {
+        for line in input_str.lines() {
+            let delta_frequency: i32 = line.parse().unwrap();
+            frequency += delta_frequency;
+            let new_entry = freq_set.insert(frequency);
+            if !new_entry { return frequency; }
+        }
+    }
+}
